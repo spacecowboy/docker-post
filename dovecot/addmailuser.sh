76bcccbce2d62d83baa5752129cbdbbb2fb8f743
Jonas Kalderstam <jonas@kalderstam.se>
2015-12-24 00:41:16 +0100

    Login with Thunderbird works, but adduser script is wrong
    It expands password...

--- dovecot/addmailuser.sh
+++ dovecot/addmailuser.sh
@@ -1,18 +1,33 @@
 #!/bin/bash -eu
 
-if [ $# -ne 3 ]; then
-  echo "Usage: addmailuser.sh bob example.com averysecretpassword"
+if [ $# -lt 3 ]; then
+  echo "Usage: addmailuser.sh [-i/u] bob example.com averysecretpassword"
   exit 1
+fi
+
+MODE="-i"
+if [ $# -eq 4 ]; then
+  MODE="$1"
+  shift
 fi
 
 # Create a password hash from the given password
 PWSTRING=$(doveadm pw -s SHA512-CRYPT -p "$2")
 
+echo "PWSTRING=$PWSTRING"
+
 VAL1="'""$1""'"
 VAL2="'""$2""'"
 VAL3="'""$PWSTRING""'"
 
-# Insert it into the database
-PGPASSWORD='Cqg2amU7SY' psql -h postgres -U mail mail -c \
-          "INSERT INTO users (name, domain, password) \
-           VALUES ($VAL1, $VAL2, $VAL3)"
+if [ "$MODE" = "-u" ]; then
+  # Update the existing entry's password
+  PGPASSWORD='Cqg2amU7SY' psql -h postgres -U mail mail -c \
+            "UPDATE users SET password=$VAL3 \
+             WHERE name=$VAL1 and domain=$VAL2"
+else
+  # Insert it into the database
+  PGPASSWORD='Cqg2amU7SY' psql -h postgres -U mail mail -c \
+            "INSERT INTO users (name, domain, password) \
+             VALUES ($VAL1, $VAL2, $VAL3)"
+fi
--- dovecot/run.sh
+++ dovecot/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -eu
 
+# Generate certs if non have been provided
+if [ ! -f /etc/ssl/certs/dovecot.pem ]; then
+  openssl req -subj "/CN=$mydomain/O=BlaBla Inc./C=US" -new -x509 -days 365 \
+          -nodes -out "/etc/ssl/certs/dovecot.pem" \
+          -keyout "/etc/ssl/private/dovecot.pem"
+fi
+
 # Set the postmaster address. This is required for lmtp to function.
 sed -i -e \
     "s/example.com/$mydomain/" \
