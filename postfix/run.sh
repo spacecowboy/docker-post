bcde4f1e11ea5d7c9b0ccbd508d2980de5037ae3
Jonas Kalderstam <jonas@kalderstam.se>
2015-12-26 01:35:30 +0100

    Add OpenDKIM signing of outgoing mail

--- postfix/run.sh
+++ postfix/run.sh
@@ -1,5 +1,32 @@
 #!/bin/bash -eu
 
+# Opendkim, if nothing has been mounted
+# Generate key
+if [ ! -f "/etc/opendkim/keys/mail.private" ]; then
+  pushd /etc/opendkim/keys
+  opendkim-genkey --subdomains --domain=$mydomain --selector=mail
+  popd
+  # Append to TrustedHosts
+  echo "*.$mydomain" >> /etc/opendkim/TrustedHosts
+  # And add to KeyTable
+  echo "mail._domainkey.$mydomain $mydomain:mail:/etc/opendkim/keys/mail.private" >> /etc/opendkim/KeyTable
+  # Append to signing table
+  echo "*@$mydomain mail._domainkey.$mydomain" >> /etc/opendkim/SigningTable
+fi
+# Print public key
+if [ -f "/etc/opendkim/keys/mail.txt" ]; then
+  echo "OpenDKIM public key:"
+  cat /etc/opendkim/keys/mail.txt
+fi
+# chown entire directory
+chown -R opendkim:opendkim /etc/opendkim/
+
+# Opendkim:
+cat /etc/opendkim.conf
+cat /etc/opendkim/TrustedHosts
+cat /etc/opendkim/SigningTable
+cat /etc/opendkim/KeyTable
+
 # Postfix does not resolv hostnames with /etc/hosts
 DOVECOTIP=$(getent hosts dovecot | awk '{ print $1 }')
 AMAVISIP=$(getent hosts amavis | awk '{ print $1 }')
@@ -27,6 +54,8 @@ grep '^[^#]' /etc/postfix/master.cf
 rm -f /etc/mailname
 echo "$myhostname" > /etc/mailname
 
+# Restart dkim
+service opendkim restart
 # Restart postfix
 postfix start
 postfix reload
